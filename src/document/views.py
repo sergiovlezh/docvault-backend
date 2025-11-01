5f9508149c14c5e2c6bbfd4ca0ac071ff1eef177
Sergio Velez <sergio.vlezh@gmail.com>
2025-11-01 17:49:13 +0000

    Add base viewsets for documents.

--- src/document/views.py
+++ src/document/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from rest_framework import permissions, viewsets
+from rest_framework.parsers import FormParser, MultiPartParser
 
-# Create your views here.
+from document.models import Document, DocumentFile, DocumentNote, Tag
+from document.serializers import (
+    DocumentFileSerializer,
+    DocumentNoteSerializer,
+    DocumentSerializer,
+    TagSerializer,
+)
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    serializer_class = DocumentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[Document]:
+        return Document.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class DocumentFileViewSet(viewsets.ModelViewSet):
+    serializer_class = DocumentFileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_queryset(self) -> QuerySet[DocumentFile]:
+        return DocumentFile.objects.filter(document__owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            uploaded_by=self.request.user, document_id=self.kwargs["document_pk"]
+        )
+
+
+class DocumentNoteViewSet(viewsets.ModelViewSet):
+    serializer_class = DocumentNoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[DocumentNote]:
+        return DocumentNote.objects.filter(document__owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user, document_id=self.kwargs["document_pk"]
+        )
