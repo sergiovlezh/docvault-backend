0da711e90ae27b71ddccf55e365b160b16688726
Sergio Velez <sergio.vlezh@gmail.com>
2025-11-08 20:43:04 +0000

    feat: Add `LoginSerializer` and `UserSerializer` for user authentication.

---
+++ src/authapi/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
+        read_only_fields = ["id", "username", "email"]
