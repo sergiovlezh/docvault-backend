d3ecc94e8d1c57c2c38330d9ed36b79e552f286e
Sergio Velez <sergio.vlezh@gmail.com>
2025-11-08 17:26:58 +0000

    refactor: Update API base path to include version prefix.

--- src/config/urls.py
+++ src/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('document.urls')),
+    path('api/v1/', include('document.urls')),
 ]
